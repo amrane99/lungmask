d1833ad43dae4fbd8191004ba3413986c409f60a
Amin Ranem <42938513+amrane99@users.noreply.github.com>
2025-06-26 23:46:27 +0200

    Update mask.py

--- lungmask/mask.py
+++ lungmask/mask.py
@@ -60,7 +60,7 @@ def apply(image, model=None, force_cpu=False, batch_size=20, volume_postprocessi
 
     timage_res = np.empty((np.append(0, tvolslices[0].shape)), dtype=np.uint8)
     
-    print(inimg_raw.size())
+    print(inimg_raw.shape)
     # timage_res = np.empty_like((np.append(0, tvolslices[0].shape)), dtype=np.uint8)
     # print(tvolslices[0].shape)
 
