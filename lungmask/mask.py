a5233223cfd71285a03f42ff6c9ad440434affff
Amin Ranem <42938513+amrane99@users.noreply.github.com>
2025-06-26 23:21:31 +0200

    Update mask.py

--- lungmask/mask.py
+++ lungmask/mask.py
@@ -66,6 +66,7 @@ def apply(image, model=None, force_cpu=False, batch_size=20, volume_postprocessi
             prediction = model(X)
             # pls = torch.max(prediction, 1)[1].detach().cpu().numpy().astype(np.uint8)
             pls = np.max(prediction.detach().cpu().numpy().astype(np.uint8), 1)[1]
+            print(pls.shape, timage_res.shape)
             timage_res = np.vstack((timage_res, pls))
 
     # postprocessing includes removal of small connected components, hole filling and mapping of small components to
