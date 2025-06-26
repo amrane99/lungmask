364fa6c97ed7ec51287739e1acfa15a13abd49f1
Amin Ranem <42938513+amrane99@users.noreply.github.com>
2025-06-26 23:55:12 +0200

    Update mask.py

--- lungmask/mask.py
+++ lungmask/mask.py
@@ -60,7 +60,7 @@ def apply(image, model=None, force_cpu=False, batch_size=20, volume_postprocessi
 
     # timage_res = np.empty((np.append(0, tvolslices[0].shape)), dtype=np.uint8)
     timage_res = np.empty_like(tvolslices[0], dtype=np.uint8)
-    # print(tvolslices[0].shape)
+    print(timage_res.shape)
 
     with torch.no_grad():
         for X in tqdm(dataloader_val):
