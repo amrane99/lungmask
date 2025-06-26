285ee5eb36742441c5679929b714fcec0e07b49a
Amin Ranem <42938513+amrane99@users.noreply.github.com>
2025-06-26 23:58:34 +0200

    Update mask.py

--- lungmask/mask.py
+++ lungmask/mask.py
@@ -59,8 +59,7 @@ def apply(image, model=None, force_cpu=False, batch_size=20, volume_postprocessi
                                                  pin_memory=False)
 
     # timage_res = np.empty((np.append(0, tvolslices[0].shape)), dtype=np.uint8)
-    timage_res = np.empty_like(tvolslices[0], dtype=np.uint8)
-    print(timage_res.shape)
+    timage_res = np.empty_like(tvolslices[0], dtype=np.uint8)[np.newaxis, ...]
 
     with torch.no_grad():
         for X in tqdm(dataloader_val):
@@ -69,6 +68,7 @@ def apply(image, model=None, force_cpu=False, batch_size=20, volume_postprocessi
             # pls = torch.max(prediction, 1)[1].detach().cpu().numpy().astype(np.uint8)
             pls = np.max(prediction.detach().cpu().numpy().astype(np.uint8), 1)[1]
             timage_res = np.vstack((timage_res, pls))
+    print(timage_res.shape, pls.shape)
 
     # postprocessing includes removal of small connected components, hole filling and mapping of small components to
     # neighbors
